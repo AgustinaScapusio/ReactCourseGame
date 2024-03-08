e82c4fb4e0d484f60c51e70df51fa4c8a1ef3927
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-08 22:14:59 +0100

    Fixed bug

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -3,7 +3,7 @@ import {forwardRef, useImperativeHandle,useRef} from 'react';
 
 const ResultModal=  forwardRef(function ResultModal({ result, targetTime,onReset,remainingTime}, ref){
 const dialog = useRef();
-const userLost= remainingTime <= 0;
+const userLost= remainingTime < 0;
 const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 const score= Math.round((1 - remainingTime/(targetTime * 1000))*100);
 
@@ -17,6 +17,7 @@ const score= Math.round((1 - remainingTime/(targetTime * 1000))*100);
     return  createPortal(
         <dialog ref={dialog}className="result-modal">
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>You won!</h2>}
             {!userLost && <h2>Your Score: {score} </h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
